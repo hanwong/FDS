407932ef18bde0ac4facf049e4de265cbce01e91
HanWoong Kim <hanationbear@gmail.com>
2016-07-30 22:42:21 +0900

    쓸데없는 슬라이더 이미지만 남았다....ㅜㅜ

--- DAY24/pizzeria/gulpfile.js
+++ DAY24/pizzeria/gulpfile.js
@@ -1,11 +1,31 @@
+// 'use strict';
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+// gulp.task('sass', function () {
+//   gulp.src('./sass/**/*.sass')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./css'));
+// });
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./sass/**/*.sass', ['sass']);
+// });
+
+
 'use strict';
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-gulp.task('sass', function () {
-  gulp.src('./sass/**/*.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.sass', ['sass']);
-});
+
+ var gulp        = require('gulp'),
+     sourcemaps  = require('gulp-sourcemaps'),
+     sass        = require('gulp-sass');
+
+ gulp.task('sass', function () {
+   return gulp.src('sass/**/*.{sass,scss}')
+   .pipe(sourcemaps.init())
+   .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+   .pipe(sourcemaps.write())
+   .pipe(gulp.dest('css'));
+ });
+  
+ gulp.task('sass:watch', function () {
+   gulp.watch('sass/**/*.{sass,scss}', ['sass']);
+ });
+ 
