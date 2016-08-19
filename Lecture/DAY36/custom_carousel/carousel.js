892d9318f789d92a2661ba0a8048ec4753ee8a4d
HanWoong Kim <hanationbear@gmail.com>
2016-08-19 10:06:16 +0900

    no message

--- Lecture/DAY36/custom_carousel/carousel.js
+++ Lecture/DAY36/custom_carousel/carousel.js
@@ -1,22 +1,23 @@
 // (function(global){
   // 'use strict';
-  var wrapper = query('.wrapper');
-  var boxs = convertArray( queryAll('.box') );
-  var movingBox = [];
-  function moveBox (argument) {
-     // body...  
-     if (false) {
-
-        movingBox = boxs.shift();
-     }
-     else if (false) {
-
-        movingBox = boxs.pop();
-     }
-
-     boxs.concat(movingBox);
+  // var wrapper = query('.wrapper');
+  var boxs = queryAll('.box');
+  var btn_left = query('.btn-left');
+  var btn_right = query('.btn-right');
+  // var movingBox = [];
+  
+  function moveLeft () {
+      boxs[4].parentNode.insertBefore(boxs[4], boxs[0]);
+      boxs = queryAll('.box');
   }
 
+  function moveRight () {
+      insertAfter(boxs[0], boxs[4]);
+      boxs = queryAll('.box');
+  }
+
+  btn_left.onclick = moveLeft;
+  btn_right.onclick = moveRight;
 
 
 
